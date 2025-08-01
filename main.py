45ccca2b0f08740aa80fb63a13c4863fcece9ecb
simran-kanase <kanasesimran8@gmail.com>
2025-08-01 16:27:06 +0530

    Initial commit

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pandas as pd
 from flask import Flask, render_template, redirect, url_for, request
 import json
 from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 
 
@@ -196,7 +198,8 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's provided port
+    app.run(host='0.0.0.0', port=port)
 
 
 
